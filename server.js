64577a2b632baa96e18a508b7b1db684d5bdf7e5
Isaac Alencar <alencarisaac3@gmail.com>
2020-04-05 14:18:13 -0300

    Adiciona uso de cors no server

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
 const requireDir = require('require-dir');
 const bodyParser = require('body-parser');
+const cors = require('cors');;
 
 //configs of the App
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Conect with database
