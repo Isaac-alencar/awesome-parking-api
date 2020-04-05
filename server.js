4e65968a801ccc6d2e668e3233277e226be9f2fb
Isaac Alencar <alencarisaac3@gmail.com>
2020-04-05 12:25:28 -0300

    Adiciona server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+const bodyParser = require('body-parser');
+
+//configs of the App
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Conect with database
+mongoose.connect("mongodb://localhost:27017/parking", {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+});
+
+requireDir('./src/models');
+
+
+app.use('/api', require('./src/routes'));
+app.listen(3001);
